a94fc0fc1ac7bd6383f27efa9208464439646901
Tahmauri <tmbo718@gmail.com>
2025-04-21 19:14:14 -0400

    Added input amount, null message, and semicolon

---
+++ project_3.s
@@ -0,0 +1,5 @@
+.data
+SpaceInput: .space 1002 #takes up to 1000 characters and the newline and null term
+null_msg: .asciiz "NULL" #message for null input
+semicolon: .asciiz ";" #semicolon for separation
+.align 2
