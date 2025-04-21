0293fd52632388d7c87a4200dda6bd9cdae65415
Tahmauri <tmbo718@gmail.com>
2025-04-21 19:36:34 -0400

    Added reading user input and input command

--- project_3.s
+++ project_3.s
@@ -11,3 +11,11 @@ strint: .space #array for integer result
 main:
     li $t0, 30 #hard coded N, loaded into $t0
     li $t1, 10 #loads 10 into $t1
+    sub $t2, $t0, $t1 #Calculates M
+
+    li $v0, 8 #reads user input
+    la $a0, SpaceInput #input characters
+    li $a1, 1002 #loads characters
+    syscall #calls command
+
+    
