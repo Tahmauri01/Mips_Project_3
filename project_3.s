87ffa7c303e6f774b4d89ddb87392bf45b5dd994
Tahmauri <tmbo718@gmail.com>
2025-04-21 19:25:02 -0400

    hard coded N, and 10

--- project_3.s
+++ project_3.s
@@ -3,3 +3,11 @@ SpaceInput: .space 1002 #takes up to 1000 characters and the newline and null te
 null_msg: .asciiz "NULL" #message for null input
 semicolon: .asciiz ";" #semicolon for separation
 .align 2
+strint: .space #array for integer result
+
+.text
+.globl main
+
+main:
+    li $t0, 30 #hard coded N, loaded into $t0
+    li $t1, 10 #loads 10 into $t1
